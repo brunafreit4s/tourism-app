ad098b8ee0dcb151a30458be1cf328d2e311eee0
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-07-20 15:03:09 -0300

    Correção nome do projeto

--- tourism_app/wsgi.py
+++ tourism_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tourism_api project.
+WSGI config for tourism_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourism_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourism_app.settings')
 
 application = get_wsgi_application()
