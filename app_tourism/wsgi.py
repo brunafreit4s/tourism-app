2c81f5543cd3637cf83aa296d0a15e837210d99d
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-07-20 15:24:31 -0300

    add rest framework

--- app_tourism/wsgi.py
+++ app_tourism/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tourism_app project.
+WSGI config for app_tourism project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourism_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_tourism.settings')
 
 application = get_wsgi_application()
