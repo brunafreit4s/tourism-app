7c6a1decdc9ff3fc6851dad4a5e83a76a6097171
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-07-20 17:44:55 -0300

    migrations

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentarios
 from avaliacoes.models import Avaliacao
+from localizacao.models import Localizacao
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
@@ -10,6 +11,7 @@ class PontoTuristico(models.Model):
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentarios)
     avaliacoes = models.ManyToManyField(Avaliacao)
+    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.nome
