0b77ad33743d5742105a5a9ac5816b88c8eef956
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-07-21 13:57:09 -0300

    add url get pontos turisticos

---
+++ core/api/serializers.py
@@ -0,0 +1,9 @@
+
+
+from rest_framework.serializers import ModelSerializer
+from core.api.viewsets import PontoTuristico
+
+class PontoTuristicoSerializer(ModelSerializer):
+    class Meta:
+        model = PontoTuristico
+        fields = ['id', 'nome', 'descricao']
--- core/models.py
+++ core/models.py
@@ -11,7 +11,7 @@ class PontoTuristico(models.Model):
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentarios)
     avaliacoes = models.ManyToManyField(Avaliacao)
-    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE)
+    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.nome
